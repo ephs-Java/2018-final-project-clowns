a47dd63b12578e8867b710be6d69b3f4a5615981
Anderson <90309776@hs-05281-macair.ad.edenpr.org>
2018-05-16 09:26:32 -0500

    ye

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -1,10 +1,60 @@
 package engine;
 
+import javax.swing.JFrame;
+
+import window.Move;
+
 public class Engine {
 
+	/*
+	 * SOURCE: https://www.ntu.edu.sg/home/ehchua/programming/java/J8d_Game_Framework.html
+	 */
+	static final int UPDATES_PER_SEC = 8; //updates per second
+	static final int UPDATES_PER_NSEC = (int) (1000000000L / UPDATES_PER_SEC); //updates per nanosecond
+	
+	static boolean gameOver = false;
+	
 	public static void main(String[] args) {
-		
+		initializeGame();
 
+	}
+	
+	public static void initializeGame () {
+		
+		/*
+		 * Creates window and creates a move object.
+		 * 
+		 */
+		JFrame window = new JFrame();
+		window.setSize(1440, 900);
+		window.setTitle("Bank Rob");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Move move = new Move();
+		window.add(move);
+		
+		window.setVisible(true);
+		
+		
+		
+		long beginTime, timeTaken, timeLeft;
+		
+		while(!gameOver) {
+			beginTime = System.nanoTime();
+			
+			timeTaken = System.nanoTime() - beginTime;
+			
+			timeLeft = (UPDATES_PER_NSEC - timeTaken) / 1000000;
+			
+			if (timeLeft > 10)
+				timeLeft = 10;
+			try {
+		         Thread.sleep(timeLeft);
+		      } catch (InterruptedException ex) { }
+			System.out.println("beginTime: " + beginTime);
+			System.out.println("timeTaken: " + timeTaken);
+			System.out.println("timeLeft: " + timeLeft);
+			
+		}
 	}
 	
 	public void run() {
@@ -16,9 +66,6 @@ public class Engine {
 		
 	}
 	
-	public void initialize () {
-		
-	}
 	
 	
 
