24be1c6cfdf1b090cb9fe175766bb60b494d52a4
Anderson <90309776@hs-05281-macair.ad.edenpr.org>
2018-05-16 09:32:34 -0500

    d

--- src/window/Move.java
+++ src/window/Move.java
@@ -3,10 +3,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
 
 public class Move extends JPanel implements ActionListener, KeyListener {
 	Timer t = new Timer(5, this);
 	double x = 0, y = 0, x2 = 0, y2 = 0;
+	ArrayList<Integer> keys = new ArrayList<Integer>();
 	public Move() {
 		t.start();
 		addKeyListener(this);
@@ -40,8 +42,19 @@ public class Move extends JPanel implements ActionListener, KeyListener {
 		y2 = 0;
 		x2 = 10;
 	}
+	
+	public void diagUpRight() {
+		x2 = 10;
+		y2 = 10;
+	}
+	
+	
 	public void keyPressed(KeyEvent e) {
+	
+		
 		int code = e.getKeyCode();
+		//System.out.println(code);
+		//keys.add(code);
 		if(code == KeyEvent.VK_UP) {
 			up();
 		}
@@ -54,8 +67,13 @@ public class Move extends JPanel implements ActionListener, KeyListener {
 		if(code == KeyEvent.VK_RIGHT) {
 			right();
 		}
+		if(code == KeyEvent.VK_RIGHT && code == KeyEvent.VK_UP ) {
+			diagUpRight();
+		}
 		
 	}
 	public void keyTyped(KeyEvent e) {}
-	public void keyReleased(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {
+		
+	}
 }
